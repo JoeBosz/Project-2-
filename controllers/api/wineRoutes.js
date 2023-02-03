54ab007c8aa0cab47438238bd9a86d59c31033f1
OGJRob <John.Rob19@aol.com>
2023-02-02 18:17:32 -0600

    Clean up the code

--- controllers/api/wineRoutes.js
+++ controllers/api/wineRoutes.js
@@ -1,4 +1,22 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { User, Wine, Order } = require('../../models');
+const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
+router.get("/", (req, res) => {
+    Wine.findAll({
+      attributes: [
+        'name',
+        'taste',
+        'price',
+        'brand',
+        'order_id',
+        'wine_id',
+      ],
+      include: [
+        {
+          model: Order,
+          attributes: ['id', 'order_date', 'order_status'],
+        }]
+    })
+  });
