8ef2b4f560d41729a2375da03dc6645cb9208d82
Tony Dandino <tdandino1005@gmail.com>
2023-02-05 14:33:14 -0600

    Pushing to Heroku

--- controllers/api/wineRoutes.js
+++ controllers/api/wineRoutes.js
@@ -1,22 +1,18 @@
-const router = require('express').Router();
-const { Wine, Order } = require('../../models');
+const router = require("express").Router();
+const { Wine, Order } = require("../../models");
 // const sequelize = require('../../config/connection');
 // const withAuth = require('../../utils/auth');
 
 router.get("/", (req, res) => {
-    Wine.findAll({
-      attributes: [
-        'name',
-        'taste',
-        'price',
-        'brand',
-        'order_id',
-        'wine_id',
-      ],
-      include: [
-        {
-          model: Order,
-          attributes: ['id', 'order_date', 'order_status'],
-        }]
-    })
+  Wine.findAll({
+    attributes: ["name", "taste", "price", "brand", "order_id", "wine_id"],
+    include: [
+      {
+        model: Order,
+        attributes: ["id", "order_date", "order_status"],
+      },
+    ],
   });
+});
+
+module.exports = router;
