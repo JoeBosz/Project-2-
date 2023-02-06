8602189662dad2f611d2ea9a38df604c219bf9fb
JoeBosz <joeboz2213@gmail.com>
2023-02-06 16:34:07 -0600

    fixed logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,15 @@
-const logout = async () => {
+const logout = async (event) => {
+  event.preventDefault();
+
   const response = await fetch("/api/users/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
-  const result = await response.json();
-  console.log(result);
 
   if (response.ok) {
     document.location.replace("/");
   } else {
-    alert(response.statusText);
+    alert("failed to logout");
   }
 };
 
